5eaea34f2d995e33758e66b64b46c34b9cf091d3
Verebik Alex <alexverebik123@gmail.com>
2023-11-26 17:00:08 +0100

    Setup room class.

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/services/RoomServiceImpl.java
@@ -0,0 +1,32 @@
+package com.epam.training.ticketservice.services;
+
+import com.epam.training.ticketservice.model.Room;
+
+import java.util.List;
+
+public class RoomServiceImpl implements RoomService {
+    @Override
+    public void createRoom(Room room) {
+
+    }
+
+    @Override
+    public void updateRoom(String roomName, Integer numberOfRows, Integer numberOfColumns) {
+
+    }
+
+    @Override
+    public void deleteRoom(String roomName) {
+
+    }
+
+    @Override
+    public String listRooms() {
+        return null;
+    }
+
+    @Override
+    public List<Room> getRooms() {
+        return null;
+    }
+}
