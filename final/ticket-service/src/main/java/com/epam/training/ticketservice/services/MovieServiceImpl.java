be35eed250e6507ca6c3ff529cf0bf2486b94ebb
Verebik Alex <alexverebik123@gmail.com>
2023-11-26 16:34:27 +0100

    Add create movie command to the app.

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/services/MovieServiceImpl.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/services/MovieServiceImpl.java
@@ -1,9 +1,25 @@
 package com.epam.training.ticketservice.services;
 
+import com.epam.training.ticketservice.model.Movie;
+import com.epam.training.ticketservice.repositories.AccountRepository;
+import com.epam.training.ticketservice.repositories.MovieRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-public class MovieServiceImpl {
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService{
+
+    private final MovieRepository movieRepository;
+
+    @Override
+    public void createMovie(Movie movie) {
+        if (movieRepository.findById(movie.getMovieName()).isEmpty()) {
+            movieRepository.save(movie);
+        }
+        else{
+            throw new IllegalArgumentException("Film already exists");
+        }
+    }
 }
